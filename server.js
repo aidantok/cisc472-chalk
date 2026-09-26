fbc4a2952c34b90d81e8ca65c590bfbd7b1e2925
aidantok <aidantok@udel.edu>
2026-09-26 00:32:43 -0400

    Fixed pre-determined hostname "localhost"

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { parse } = require("url");
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
+const hostname = "0.0.0.0";
 const port = parseInt(process.env.PORT, 10) || 3000;
 
 const app = next({ dev, hostname, port });
